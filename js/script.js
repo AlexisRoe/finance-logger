bc25e151a88de3580bb288dabce2bf718148b04e
Alexis Roehrling <A.Roehrling@gmx.de>
2021-01-28 16:57:24 +0100

    Add getting form values

--- js/script.js
+++ js/script.js
@@ -1,2 +1,10 @@
-var test = "hello";
-console.log(test);
+// form elements
+var form = document.querySelector(".new-item-form");
+var type = document.querySelector("#type");
+var toFrom = document.querySelector("#toFrom");
+var details = document.querySelector("#details");
+var amount = document.querySelector("#amount");
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    console.log(type.value, toFrom.value, details.value, amount.valueAsNumber);
+});
--- js/script.ts
+++ js/script.ts
@@ -1,2 +1,11 @@
-const test = "hello";
-console.log(test)
+// form elements
+const form = document.querySelector(".new-item-form") as HTMLFormElement;
+const type = document.querySelector("#type") as HTMLSelectElement;
+const toFrom = document.querySelector("#toFrom") as HTMLInputElement;
+const details = document.querySelector("#details") as HTMLInputElement;
+const amount  = document.querySelector("#amount") as HTMLInputElement;
+
+form.addEventListener("submit", (event: Event) => {
+    event.preventDefault();
+    console.log(type.value, toFrom.value, details.value, amount.valueAsNumber)
+})
